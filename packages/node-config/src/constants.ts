0415ce5fb931d8373a08054034a58aa9d6fa15ad
stringang <14845458+stringang@users.noreply.github.com>
2021-07-31 10:04:14 +0800

    macos wip

---
+++ packages/node-config/src/constants.ts
@@ -0,0 +1,9 @@
+/**
+ * In the event NODE_ENV isn't set, this will be used
+ */
+export const DEFAULT_ENVIRONMENT: string = 'development'
+
+/**
+ * Local folder, relative to cwd, to find configs
+ */
+export const DEFAULT_CONFIG_PATH: string = 'config'
